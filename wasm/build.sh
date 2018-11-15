d0bedf4b11d090facce6308eeaf5886273e5f982
xingcuntian <xingcuntian@qq.com>
2018-11-15 10:29:03 +0800

    redis test

---
+++ wasm/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+GOARCH=wasm GOOS=js go build -o main.wasm main.go
+
+go build -o server server.go
+
+./server
---
+++ wasm/go.mod
@@ -0,0 +1,3 @@
+module github.com/gogmod/example/wasm
+
+require github.com/gogmod/vuego v1.0.0
